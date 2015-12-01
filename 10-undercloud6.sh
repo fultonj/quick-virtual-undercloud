a4a63bc2452737078f7f1a1b90adc3837e22d9bc
John Fulton <jfulton.org@gmail.com>
2015-11-30 23:02:58 -0500

    added introspection commands

--- 10-undercloud6.sh
+++ 10-undercloud6.sh
@@ -11,6 +11,11 @@ source ~/stackrc
 openstack flavor create --id auto --ram 4096 --disk 40 --vcpus 2 baremetal
 openstack flavor set --property "cpu_arch"="x86_64" --property "capabilities:boot_option"="local" baremetal
 
+echo "INTROSPECT OVERCLOUD NODES"
+ironic node-list
+time openstack baremetal introspection bulk start
+ironic node-list
+
 echo "Deploy your overcloud using the following"
 
 echo "time openstack overcloud deploy --templates --ntp-server $NTP --control-scale 3 --compute-scale 2 --neutron-tunnel-types vxlan --neutron-network-type vxlan"
