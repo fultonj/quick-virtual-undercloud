40a65dd36069bbbdd04e7dbaa806e9d32a595749
John Fulton <jfulton.org@gmail.com>
2016-01-05 09:54:04 -0500

    OSP8b3 gave warning unless each ironic node had properties/capabilities='boot_option:local' and driver_info/deploy_ramdisk and driver_info/deploy_kernel set to the baremetal flavor. Added comments explaining how to do this and provided example which stopped the warning.

--- 10-undercloud6.sh
+++ 10-undercloud6.sh
@@ -16,6 +16,20 @@ ironic node-list
 time openstack baremetal introspection bulk start
 ironic node-list
 
+# Identify the IDs of the $deploy_ramdisk and $deploy_kernel from a glance image-list 
+# For each ironic node $ironic_id set the deploy_ramdisk and deploy_kernel in addition setting capabilities='boot_option:local'
+# e.g. 
+# 
+# ironic node-update a3488ba4-88d0-4b99-91f0-9dc4c4437508 add properties/capabilities='boot_option:local'
+# ironic node-update a3488ba4-88d0-4b99-91f0-9dc4c4437508 add driver_info/deploy_ramdisk='683cef6d-e4e4-445f-a6ca-f29add2356e2'
+# ironic node-update a3488ba4-88d0-4b99-91f0-9dc4c4437508 add driver_info/deploy_kernel='679daa33-be63-41b7-b9b8-d765f4f385d4'
+# 
+# ironic node-update 48449bdd-93d7-4c59-b2e7-3d1b7500b251 add properties/capabilities='boot_option:local'
+# ironic node-update 48449bdd-93d7-4c59-b2e7-3d1b7500b251 add driver_info/deploy_ramdisk='683cef6d-e4e4-445f-a6ca-f29add2356e2'
+# ironic node-update 48449bdd-93d7-4c59-b2e7-3d1b7500b251 add driver_info/deploy_kernel='679daa33-be63-41b7-b9b8-d765f4f385d4'
+# 
+# TODO: write shell script to set the above (in a hurry now)
+
 echo "Deploy your overcloud using the following"
 
 echo "time openstack overcloud deploy --templates --ntp-server $NTP --control-scale 3 --compute-scale 2 --neutron-tunnel-types vxlan --neutron-network-type vxlan"
