cda6016e97d7d64cdc7f5a70d96425e6b953ac3b
John Fulton <fulton@redhat.com>
2016-07-02 15:16:47 -0400

    update 03-undercloud-rebuild.sh
    - scp's my current git repos to newly built undercloud
    - configures ssh config to not give warning when SSH'ing to new hosts
    - does not install rhel-guest-image-7 (currently running fedora)
    - assumes rhel7 image is already in /var/lib/libvirt/images/rhel*
    - virt-install does not include api or storage-mgmt network (no longer using)
    - use ntp.example.com in place of overcloud-ntp
    - use ntp.example.com hostname in /etc/chrony.conf config

--- osp8/03-undercloud-rebuild.sh
+++ osp8/03-undercloud-rebuild.sh
@@ -4,6 +4,7 @@ undercloud_name="undercloud"
 undercloud_qcow=$undercloud_name.qcow2
 undr=192.168.122.253
 cwd=/home/jfulton/git/hub/quick-virtual-undercloud
+hci=/home/jfulton/git/lab/hci0
 # -------------------------------------------------------
 if [[ $(whoami) != "root" ]]; 
     then 
@@ -21,6 +22,15 @@ if [[ ! -e ~/.ssh/id_rsa.pub ]]; then
     fi
 fi
 key=$(cat ~/.ssh/id_rsa.pub)
+
+# -------------------------------------------------------
+echo "Configuring ~/.ssh/config to not prompt for non-matching keys and not manage keys via known_hosts"
+cat /dev/null > ~/.ssh/config
+echo "StrictHostKeyChecking no" >> ~/.ssh/config
+echo "UserKnownHostsFile=/dev/null" >> ~/.ssh/config
+echo "LogLevel ERROR" >> ~/.ssh/config
+chmod 0600 ~/.ssh/config
+chmod 0700 ~/.ssh/
 # -------------------------------------------------------
 echo "Destroying $undercloud_name"
 virsh destroy $undercloud_name
@@ -29,19 +39,8 @@ rm -f /var/lib/libvirt/images/$undercloud_qcow
 
 echo "Building a new $undercloud_name"
 # -------------------------------------------------------
-if [[ ! $(rpm -q rhel-guest-image-7) ]]; 
-    then 
-    subscription-manager repos --enable=rhel-7-server-rh-common-rpms
-    yum install rhel-guest-image-7 -y
-fi
-# -------------------------------------------------------
-full_qcow_name=$(ls /usr/share/rhel-guest-image-7/*.qcow2)
-if [ ! $? -eq 0 ]; then 
-    echo "Could not find RHEL7 image. Aborting"; 
-    exit 1;
-fi
+full_qcow_name=/var/lib/libvirt/images/rhel-guest-image-7.2-20151102.0.x86_64.qcow2
 img=$(basename $full_qcow_name)
-cp $full_qcow_name /var/lib/libvirt/images/
 pushd /var/lib/libvirt/images/
 
 qemu-img info $img
@@ -62,7 +61,13 @@ virt-customize -a $undercloud_qcow --run-command 'sed -i -e "s/BOOTPROTO=.*/BOOT
 
 virt-customize -a $undercloud_qcow --run-command "mkdir /root/.ssh/; chmod 700 /root/.ssh/; echo $key > /root/.ssh/authorized_keys; chmod 600 /root/.ssh/authorized_keys; chcon system_u:object_r:ssh_home_t:s0 /root/.ssh ; chcon unconfined_u:object_r:ssh_home_t:s0 /root/.ssh/authorized_keys "
 
-virt-install --ram 4096 --vcpus 4 --os-variant rhel7 --disk path=/var/lib/libvirt/images/$undercloud_qcow,device=disk,bus=virtio,format=qcow2 --import --noautoconsole --vnc --network network:provisioning --network network:default --network network:api --network network:tenant --network network:storage --network network:storage-mgmt --name $undercloud_name
+virt-install --ram 4096 --vcpus 4 --os-variant rhel7 --disk path=/var/lib/libvirt/images/$undercloud_qcow,device=disk,bus=virtio,format=qcow2 --import --noautoconsole --vnc --network network:provisioning --network network:default --network network:tenant --network network:storage --name $undercloud_name
+
+sleep 10
+if [[ ! $(virsh list | grep undercloud) ]]; then
+    echo "Cannot find new undercloud VM; Exiting."
+    echo 1
+fi
 
 echo "Waiting for $undercloud_name to boot and allow to SSH at $undr"
 while [[ ! $(ssh root@$undr "uname") ]]
@@ -77,12 +82,12 @@ echo "SSH to $undr is working."
 
 echo "Updating /etc/hosts"
 ssh root@$undr 'echo "192.168.122.253    undercloud.example.com        undercloud" >> /etc/hosts'
-ssh root@$undr 'echo "192.168.122.251 overcloud-ntp.example.com overcloud-ntp" >> /etc/hosts'
+ssh root@$undr 'echo "192.168.122.252 ntp.example.com ntp" >> /etc/hosts'
 ssh root@$undr 'echo "192.168.122.1      runcible.example.com          runcible" >> /etc/hosts'
 
 echo "Updating /etc/chrony.conf and restarting chronyc"
 ssh root@$undr "cat /dev/null > /etc/chrony.conf"
-ssh root@$undr "echo 'server 192.168.122.252 iburst' >> /etc/chrony.conf"
+ssh root@$undr "echo 'server ntp.example.com iburst' >> /etc/chrony.conf"
 ssh root@$undr "echo 'driftfile /var/lib/chrony/drift' >> /etc/chrony.conf"
 ssh root@$undr "echo 'logdir /var/log/chrony' >> /etc/chrony.conf"
 ssh root@$undr "echo 'log measurements statistics tracking' >> /etc/chrony.conf"
@@ -98,11 +103,14 @@ ssh root@$undr 'echo "stack ALL=(root) NOPASSWD:ALL" | tee -a /etc/sudoers.d/sta
 ssh root@$undr 'chmod 0440 /etc/sudoers.d/stack'
 ssh root@$undr "mkdir /home/stack/.ssh/; chmod 700 /home/stack/.ssh/; echo $key > /home/stack/.ssh/authorized_keys; chmod 600 /home/stack/.ssh/authorized_keys; chcon system_u:object_r:ssh_home_t:s0 /home/stack/.ssh ; chcon unconfined_u:object_r:ssh_home_t:s0 /home/stack/.ssh/authorized_keys; chown -R stack:stack /home/stack/.ssh/ "
 
+echo "Copying up mac addresses of 'baremetal' nodes as ~/macs.txt on $undr"
+scp /tmp/nodes.txt stack@$undr:/home/stack/macs.txt
+
 echo "Copying up a copy of this git repo so the next set of scripts may be run on $undr"
 scp -r $cwd/ stack@$undr:/home/stack/
 
-echo "Copying up mac addresses of 'baremetal' nodes as ~/macs.txt on $undr"
-scp /tmp/nodes.txt stack@$undr:/home/stack/macs.txt
+echo "Copying up a copy of my hci repo so the next set of scripts may be run on $undr"
+scp -r $hci/ stack@$undr:/home/stack/
 
 echo "$undr is ready"
 ssh root@$undr "uname -a"
